1c13f89cfab91f011a913f4c151da43d5fe58131
clayton-schneider <schneiderc2016@gmail.com>
2024-01-11 12:06:32 -0500

    setup cookies to pass form data through route

--- heat-stack/app/routes/_heat+/inputs2.tsx
+++ heat-stack/app/routes/_heat+/inputs2.tsx
@@ -1,5 +1,23 @@
+import type { LoaderFunctionArgs } from '@remix-run/node' 
+import { json } from "@remix-run/node"
+import { useLoaderData } from '@remix-run/react'
 import { CurrentHeatingSystem } from '../../components/ui/heat/CaseSummaryComponents/CurrentHeatingSystem.tsx'
+import { getSession } from '../homes.tsx'
+
+export async function loader({request}: LoaderFunctionArgs) {
+	const session = await getSession(request.headers.get("Cookie"))	
+
+	const formData = session.get("formData")
+
+	console.log("from inputs2 loader: ", formData)
+
+	return json(formData)
+}
+
 export default function Inputs2() {
+	const formData = useLoaderData<typeof loader>()
+
+	console.log("from client useLoader: ", formData)
 	return (
 		<div>
 			<CurrentHeatingSystem />
--- heat-stack/app/routes/homes.tsx
+++ heat-stack/app/routes/homes.tsx
@@ -1,15 +1,53 @@
 import type {
-  ActionFunctionArgs,
+	ActionFunctionArgs,
 } from "@remix-run/node";
-import { redirect } from "@remix-run/node"
+import { redirect, createCookieSessionStorage } from "@remix-run/node"
 
 export async function action({
 	request
 }: ActionFunctionArgs) {
+
+	const session = await getSession(
+		request.headers.get("Cookie")
+	)
+
 	const formData = await request.formData()
 
-	formData.forEach(d => console.log(d))
-	
-	return redirect("/inputs1")
+	// can't just dump formData
+	const returnData = {}
+	formData.forEach((val, key) => returnData[key] = val)  
+
+	session.set("formData", returnData)
+	console.log("from homes action: ", session.data)
+
+
+	return redirect("/inputs2", {
+		headers: {
+			"Set-Cookie": await commitSession(session)
+		}
+	})
 }
 
+
+// added to make cookie session work
+
+type SessionData = {
+	// we can set this to an object of that represents the shape of the form data
+	formData: any
+}
+
+type SessionFlashData = {
+	error: string
+}
+
+const { getSession, commitSession, destroySession } =
+	createCookieSessionStorage<SessionData, SessionFlashData>(
+		{
+			// a Cookie from `createCookie` or the CookieOptions to create one
+			cookie: {
+				name: "__session",
+			},
+		}
+	);
+
+export { getSession, commitSession, destroySession };
